33a092f02c077833b7109f29320902b08f95fa88
Yassine H.M <68260654+yassine-hamadou@users.noreply.github.com>
2023-05-25 22:46:41 +0000

    Dashboadr winForm

---
+++ DashboradWinForm/DesignerForm1.cs
@@ -0,0 +1,16 @@
+﻿using DevExpress.DashboardCommon;
+
+namespace DashboradWinForm
+{
+    public partial class DesignerForm1 : DevExpress.XtraBars.Ribbon.RibbonForm
+    {
+        public DesignerForm1()
+        {
+            InitializeComponent();
+            dashboardDesigner.DataSourceWizard.SqlWizardSettings.EnableCustomSql = true;
+            dashboardDesigner.CreateRibbon();
+            dashboardDesigner.LoadDashboard(@"Dashboards\dashboard1.xml");
+            
+        }
+    }
+}
---
+++ DashboradWinForm/Program.cs
@@ -0,0 +1,25 @@
+﻿using DevExpress.Skins;
+using DevExpress.UserSkins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace DashboradWinForm
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {//Enable custom sql queries for the dashboard data source wizard
+            
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            DesignerForm1 designer1 = new DesignerForm1();
+            Application.Run(designer1);
+        }
+    }
+}
